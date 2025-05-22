6d60973d850b2a6584b90a09ef1b0f8bac70c750
Elisha Stephen <129074116+ST10291541@users.noreply.github.com>
2025-05-22 11:06:23 +0200

    Fixed contact us

--- app/src/main/java/vcmsa/projects/chocui/MainActivity.kt
+++ app/src/main/java/vcmsa/projects/chocui/MainActivity.kt
@@ -9,15 +9,11 @@ import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         // Setup contacts button click listener
         findViewById<MaterialButton>(R.id.contactsButton).setOnClickListener {
